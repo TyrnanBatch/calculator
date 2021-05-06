0725b737105cc324ee74c084136f257b8a60c2b4
Tyrnan Batch <51964350+TyrnanBatch@users.noreply.github.com>
2021-05-06 21:28:55 +0100

    I'm not entirely sure why this doesn't work

--- calculator.py
+++ calculator.py
@@ -3,12 +3,71 @@ calculationInput = input("Input calculation: ")
 calculation = calculationInput.split(" ")
 
 calculataionLen = len(calculation)
-calculataionLen = calculataionLen / 2 + 0.5
+calculataionLen = calculataionLen / 2 - 0.5 
 print(calculataionLen)
 
 count = 0
+result = 0
 
-while count <= calculataionLen:
-    count += 1
-    if count % 2 != 0:
-        print(calculation[(count)])
+if calculataionLen == 1:    
+    while count < calculataionLen:
+        count += 1
+        if count % 2 != 0:
+            print(calculation[(count)])
+else:
+    while count <= calculataionLen:
+        count += 1
+        if count % 2 != 0:
+            print(calculation[(count)])
+
+if calculation[(count)] == "+":
+    placeHolder = int(calculation[count - 1] + int(calculation[count + 1]
+    if calculation[count - 6] == "+":
+        result = int(result) + int(placeHolder)
+    elif calculation[count - 6] == "-":
+        result = int(result) - int(placeHolder)
+    elif calculation[count - 6] == "*":
+        result = int(result) * int(placeHolder)
+    elif calculation[count - 6] == "/":
+        result = int(result) / int(placeHolder)
+    else:
+        result = int(result) + int(placeHolder)
+
+if calculation[(count)] == "-":
+    placeHolder = (calculation[count - 1]) - (calculation[count + 1])
+    if calculation[count - 6] == "+":
+        result = int(result) + int(placeHolder)
+    elif calculation[count - 6] == "-":
+        result = int(result) - int(placeHolder)
+    elif calculation[count - 6] == "*":
+        result = int(result) * int(placeHolder)
+    elif calculation[count - 6] == "/":
+        result = int(result) / int(placeHolder)
+    else:
+        result = int(result) + int(placeHolder)
+
+if calculation[(count)] == "*":
+    placeHolder = (calculation[count - 1]) * (calculation[count + 1])
+    if calculation[count - 6] == "+":
+        result = int(result) + int(placeHolder)
+    elif calculation[count - 6] == "-":
+        result = int(result) - int(placeHolder)
+    elif calculation[count - 6] == "*":
+        result = int(result) * int(placeHolder)
+    elif calculation[count - 6] == "/":
+        result = int(result) / int(placeHolder)
+    else:
+        result = int(result) + int(placeHolder)#
+
+if calculation[(count)] == "/":
+    placeHolder = (calculation[count - 1] / (calculation[count + 1])
+    if calculation[count - 6] == "+":
+        result = int(result) + int(placeHolder)
+    elif calculation[count - 6] == "-":
+        result = int(result) - int(placeHolder)
+    elif calculation[count - 6] == "*":
+        result = int(result) * int(placeHolder)
+    elif calculation[count - 6] == "/":
+        result = int(result) / int(placeHolder)
+    else:
+        result = int(result) + int(placeHolder)
