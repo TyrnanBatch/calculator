803b72f9700ace5cad8791bc9c7c5517c7c5ab2f
Tyrnan Batch <51964350+TyrnanBatch@users.noreply.github.com>
2021-09-17 22:31:45 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,34 +1,45 @@
-import re
+calculation_input = '100*3+10/2'
 
-calculation_input = input('Input calc :')
-result = re.split('(\W)', calculation_input)
+def calculator(calculation_input):
+    import re
+    result = re.split('(\W)', calculation_input)
 
-result_length = len(result)
+    result_length = len(result)
 
-for count in range(result_length):
-    if result[count] == '*':
-        multiply_result = int(result[count-1]) * int(result[count+1])
-        result[count] = multiply_result
-        del result[count-1]
-        del result[count]
-    if result[count] == '/':
-        divide_result = int(result[count-1]) / int(result[count+1])
-        result[count] = divide_result
-        del result[count-1]
-        del result[count]
-    print(count)
+    count = 0
+    for char in range(result_length):
+        if result[count] == '*':
+            multiply_result = int(result[count-1]) * int(result[count+1])
+            result[count] = multiply_result
+            del result[count-1]
+            del result[count]
+            count = 0
+        if result[count] == '/':
+            divide_result = int(result[count-1]) / int(result[count+1])
+            result[count] = divide_result
+            del result[count-1]
+            del result[count]
+            count = 0
+        if len(result) == 1:
+            return result[0]
+        count += 1
 
-result_length = len(result)
+    count = 0
+    for char in range(result_length):
+        if result[count] == '+':
+            plus_result = int(result[count-1]) + int(result[count+1])
+            result[count] = plus_result
+            del result[count-1]
+            del result[count]
+            count = 0
+        if result[count] == '-':
+            minus_result = int(result[count-1]) - int(result[count+1])
+            result[count] = minus_result
+            del result[count-1]
+            del result[count]
+            count = 0
+        if len(result) == 1:
+            return result[0]
+        count += 1
 
-for count in range(result_length):
-    if result[count] == '+':
-        multiply_result = int(result[count-1]) + int(result[count+1])
-        result[count] = multiply_result
-        del result[count-1]
-        del result[count]
-    if result[count] == '-':
-        divide_result = int(result[count-1]) - int(result[count+1])
-        result[count] = divide_result
-        del result[count-1]
-        del result[count]
-    print(count)
+print(calculator(calculation_input))
